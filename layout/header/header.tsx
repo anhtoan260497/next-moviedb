313da1f234436b78533e8f3aad06dcf565d84236
anhtoan260497 <anhtoan260497@gmail.com>
2023-03-05 13:51:06 +0700

    add header desktop

---
+++ layout/header/header.tsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import styles from "./Header.module.scss";
+import Image from "next/image";
+
+const Header = () => {
+
+  const menu = [{
+    id: 0,
+    name: 'Movies',
+    path: '/movies',
+    children: [{
+      name: 'Popular',
+      path: '/popular'
+    }, {
+      name: 'Now Playing',
+      path: '/nowplaying'
+    }, {
+      name: 'Upcoming',
+      path: '/upcoming'
+    }, {
+      name: 'Top Rated',
+      path: '/toprated'
+    }]
+  }, {
+    id: 1,
+    name: 'TV Shows',
+    path: '/tvshows',
+    children: [{
+      name: 'Popular',
+      path: '/popular'
+    }, {
+      name: 'Airing Today',
+      path: '/airingtoday'
+    }, {
+      name: 'on TV',
+      path: '/ontv'
+    }, {
+      name: 'Top Rated',
+      path: '/toprated'
+    }]
+  }, {
+    id: 2,
+    name: 'People',
+    path: '/people',
+    children: [{
+      name: 'Popular People',
+      path: '/popularpeople'
+    }]
+  }, {
+    id: 3,
+    name: 'More',
+    path: '/more',
+    children: [{
+      name: 'Discussion',
+      path: '/discussion'
+    }, {
+      name: 'Leaderboard',
+      path: '/leaderboard'
+    }, {
+      name: 'Support',
+      path: '/support'
+    }, {
+      name: 'API',
+      path: '/api'
+    }]
+  }]
+
+  const [hoverItem, setHoverItem] = useState(-1)
+
+  const renderMenuItem = () => {
+    return menu.map(item => {
+      return (
+        <div className={styles.menuItem}>
+          <a className={styles.menuItemTitle} onMouseEnter={() => setHoverItem(item.id)}>{item.name}</a>
+          {hoverItem === item.id ? <ul className={styles.menuChildItemList} onMouseEnter={()=>setHoverItem(item.id)} onMouseLeave={()=>setHoverItem(-1)}>
+            {item.children.map(childItem => {
+              return <li className={styles.menuChildItem}><a>{childItem.name}</a></li>
+            })}
+          </ul> : ''}
+        </div>
+      )
+    })
+  }
+
+
+  return (
+    <div className={styles.containerFluid}>
+      <div className={styles.container}>
+        <div className={styles.leftContainer}>
+          <Image
+            src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+            width={154}
+            height={20}
+            alt="logo"
+          />
+
+          <div className={styles.menuContainer} onMouseLeave={()=>setHoverItem(-1)}>
+           {renderMenuItem()}
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
