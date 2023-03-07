bf40aa36ec750ab4ee65ccaead0119cb2f3fc903
anhtoan260497 <anhtoan260497@gmail.com>
2023-03-07 15:53:34 +0700

    add trending homepage desktop

--- components/MoviesSlide/MovieSlide.tsx
+++ components/MoviesSlide/MovieSlide.tsx
@@ -1,6 +1,6 @@
 import { MovieList } from '@/pages';
 import { RootState } from '@/store/store';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import MovieItem from '../MovieItem/MovieItem';
 import styles from './MovieSlide.module.scss'
@@ -9,15 +9,24 @@ import styles from './MovieSlide.module.scss'
 function MovieSlide() {
 
     const movies: any = useSelector<RootState>(state => state.movieSlice.trending)
-    console.log(movies)
+    const containerRef = useRef<HTMLInputElement>(null)
+    console.log(containerRef.current)
     const renderMovieItem = () => {
     
         return movies.map((item: MovieList) => <MovieItem poster={item.poster_path} date={item.release_date}
             name={item.original_title} voteAverage={item.vote_average} />)
     }
 
+    useEffect(() => {
+       setTimeout(() => {
+        if(containerRef.current != null){
+            containerRef.current.classList.add(styles.active)
+         }
+       }, 200);
+    })
+
     return (
-        <div className={styles.container}>
+        <div className={styles.container} ref={containerRef}>
             <div className={styles.scrollContainer}>
                 {renderMovieItem()}
             </div>
