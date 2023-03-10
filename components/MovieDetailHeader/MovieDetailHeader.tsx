07f186e7f619f28ecfcb0ce3b2084d4e4a198c4d
anhtoan260497 <anhtoan260497@gmail.com>
2023-03-10 19:45:42 +0700

    header movie detail desktop

--- components/MovieDetailHeader/MovieDetailHeader.tsx
+++ components/MovieDetailHeader/MovieDetailHeader.tsx
@@ -1,17 +1,66 @@
 import { calculateRuntime, progressColor } from '@/helper';
 import { CircularProgress, Icon } from '@mui/material';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './MovieDetailHeader.module.scss'
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import { MovieInfo } from '@/pages/[type]/[id]';
+import { CrewItem, getCreditMovie, getTrailersMovie, MovieInfo, VideoItem } from '@/features/MovieInfoSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/store/store';
+import { useRouter } from 'next/router';
+import { ThunkDispatch } from '@reduxjs/toolkit';
+import { setChoosingTrailer, setModalTrailers } from '@/features/MovieSlice';
 
-function MovieDetailHeader({ backdrop_path, poster_path, original_title = '', 
-adult, release_date, genres, runtime = 0, vote_average = 0, overview, tagline}: MovieInfo) {
-    const voteAverage = 3.0
+function MovieDetailHeader() {
+    const movieInfoData = useSelector<RootState, MovieInfo>(state => state.movieInfoSlice.info)
+    const { backdrop_path, poster_path, original_title = '', original_name = '', original_language, first_air_date, episode_run_time = [],
+        adult, release_date, genres, runtime = 0, vote_average = 0, overview, tagline, id, origin_country = [] } = movieInfoData
+    const trailerVideo = useSelector<RootState, VideoItem>(state => state.movieInfoSlice.trailer)
+    const crews = useSelector<RootState, CrewItem[]>(state => state.movieInfoSlice.crew)
+
+    const dispatch = useDispatch<ThunkDispatch<any, any, any>>()
+
+    const router = useRouter()
 
     const renderGenres = () => {
         return (genres?.map(item => item.name))?.join(', ')
+    }
+
+    const renderCrew = () => {
+        const crewDirector = crews.filter(item => item.job.includes('Creator') || item.job.includes('Director') || item.job.includes('Screenplay') )
+        const crewDirectorLimit = crewDirector.filter((item,key) => key < 6)
+        return crewDirectorLimit.map(item => {
+            return (
+                <div className={styles.directorInfo}>
+                <p className={styles.directorName}>{item.name}</p>
+                <p className={styles.directorCast}>{item.job}</p>
+            </div>
+            )
+        })
+    }
+
+    useEffect(() => {
+        const { type, id } = router.query
+        dispatch(getTrailersMovie({
+            type: type,
+            id: id,
+        }))
+        dispatch(getCreditMovie({
+            type: type,
+            id: id,
+        }))
+    }, [])
+
+    const openTrailer = () => {
+        dispatch(setChoosingTrailer({
+            poster_path,
+            name: original_title,
+            video_name: trailerVideo.name,
+            id,
+            path: trailerVideo.key
+        }))
+
+        dispatch(setModalTrailers(true))
     }
 
     return (
@@ -20,12 +69,12 @@ adult, release_date, genres, runtime = 0, vote_average = 0, overview, tagline}: 
             <div className={styles.container}>
                 <Image className={styles.poster} src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${poster_path}`} alt={original_title} width={300} height={450} />
                 <div className={styles.movieInfoContainer}>
-                    <h1>{original_title}</h1>
+                    <h1>{original_title || original_name}</h1>
                     <div className={styles.info}>
                         <div className={styles.certification}>{adult ? 'M' : 'PG-13'}</div>
-                        •<p>{release_date} (US)</p>
+                        •<p className={styles.languages}>{release_date || first_air_date} {`(${origin_country[0] || original_language})`}</p>
                         •<p>{renderGenres()}</p>
-                        •<p>{calculateRuntime(runtime)}</p>
+                        •{<p>{runtime ? calculateRuntime(runtime) : calculateRuntime(episode_run_time[0])}</p>}
                     </div>
                     <div className={styles.scoreAndTrailer}>
                         <div className={styles.userScoreContainer}>
@@ -37,10 +86,10 @@ adult, release_date, genres, runtime = 0, vote_average = 0, overview, tagline}: 
                             </div>
                             <p className={styles.userScoreLabel}>User<br />Score</p>
                         </div>
-                        <div className={styles.playTrailer}>
+                        {trailerVideo.id && <button className={styles.playTrailer} onClick={openTrailer}>
                             <Icon component={PlayArrowIcon} />
                             Play Trailer
-                        </div>
+                        </button>}
                     </div>
                     <p className={styles.quotes}>{tagline}</p>
                     <div className={styles.overviewContainer}>
@@ -48,22 +97,7 @@ adult, release_date, genres, runtime = 0, vote_average = 0, overview, tagline}: 
                         <p className={styles.overview}>{overview}</p>
                     </div>
                     <div className={styles.directorContainer}>
-                        <div className={styles.directorInfo}>
-                            <p className={styles.directorName}>Nicholas D. Johnson</p>
-                            <p className={styles.directorCast}>Director, Screenplay</p>
-                        </div>
-                        <div className={styles.directorInfo}>
-                            <p className={styles.directorName}>Nicholas D. Johnson</p>
-                            <p className={styles.directorCast}>Director, Screenplay</p>
-                        </div>
-                        <div className={styles.directorInfo}>
-                            <p className={styles.directorName}>Nicholas D. Johnson</p>
-                            <p className={styles.directorCast}>Director, Screenplay</p>
-                        </div>
-                        <div className={styles.directorInfo}>
-                            <p className={styles.directorName}>Nicholas D. Johnson</p>
-                            <p className={styles.directorCast}>Director, Screenplay</p>
-                        </div>
+                    {renderCrew()}
                     </div>
                 </div>
             </div>
--- store/store.ts
+++ store/store.ts
@@ -1,20 +1,24 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import {toggleSlice} from '@/features/ToggleSlice'
 import {movieSlice} from '@/features/MovieSlice';
+import {movieInfoSlice} from '@/features/MovieInfoSlice';
 
 export interface Reducers {
     toggleSlice : ReturnType<typeof toggleSlice.reducer>
     movieSlice : ReturnType<typeof movieSlice.reducer>
+    movieInfoSlice : ReturnType<typeof movieInfoSlice.reducer>
 }
 
 const reducers = combineReducers({
    toggleSlice : toggleSlice.reducer,
-   movieSlice : movieSlice.reducer
+   movieSlice : movieSlice.reducer,
+   movieInfoSlice : movieInfoSlice.reducer,
 })
 
 const store = configureStore({
     reducer : reducers,
     devTools : true,
+    
 })
 
 export default store
