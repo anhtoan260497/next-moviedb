d32a5ba2a50ecc1fea1b69a3f67da03c59e12871
anhtoan260497 <anhtoan260497@gmail.com>
2023-03-10 20:50:45 +0700

    mobile header deatail

--- components/MovieDetailHeader/MovieDetailHeader.tsx
+++ components/MovieDetailHeader/MovieDetailHeader.tsx
@@ -10,6 +10,7 @@ import { RootState } from '@/store/store';
 import { useRouter } from 'next/router';
 import { ThunkDispatch } from '@reduxjs/toolkit';
 import { setChoosingTrailer, setModalTrailers } from '@/features/MovieSlice';
+import clsx from 'clsx';
 
 function MovieDetailHeader() {
     const movieInfoData = useSelector<RootState, MovieInfo>(state => state.movieInfoSlice.info)
@@ -27,14 +28,14 @@ function MovieDetailHeader() {
     }
 
     const renderCrew = () => {
-        const crewDirector = crews.filter(item => item.job.includes('Creator') || item.job.includes('Director') || item.job.includes('Screenplay') )
-        const crewDirectorLimit = crewDirector.filter((item,key) => key < 6)
+        const crewDirector = crews.filter(item => item.job.includes('Creator') || item.job.includes('Director') || item.job.includes('Screenplay'))
+        const crewDirectorLimit = crewDirector.filter((item, key) => key < 6)
         return crewDirectorLimit.map(item => {
             return (
                 <div className={styles.directorInfo}>
-                <p className={styles.directorName}>{item.name}</p>
-                <p className={styles.directorCast}>{item.job}</p>
-            </div>
+                    <p className={styles.directorName}>{item.name}</p>
+                    <p className={styles.directorCast}>{item.job}</p>
+                </div>
             )
         })
     }
@@ -64,44 +65,49 @@ function MovieDetailHeader() {
     }
 
     return (
-        <div className={styles.containerFluid} style={{ backgroundImage: `url(https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${backdrop_path})` }}>
-            <div className={styles.background}></div>
-            <div className={styles.container}>
-                <Image className={styles.poster} src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${poster_path}`} alt={original_title} width={300} height={450} />
-                <div className={styles.movieInfoContainer}>
-                    <h1>{original_title || original_name}</h1>
-                    <div className={styles.info}>
-                        <div className={styles.certification}>{adult ? 'M' : 'PG-13'}</div>
-                        •<p className={styles.languages}>{release_date || first_air_date} {`(${origin_country[0] || original_language})`}</p>
-                        •<p>{renderGenres()}</p>
-                        •{<p>{runtime ? calculateRuntime(runtime) : calculateRuntime(episode_run_time[0])}</p>}
-                    </div>
-                    <div className={styles.scoreAndTrailer}>
-                        <div className={styles.userScoreContainer}>
-                            <div className={styles.progress}>
-                                <p className={styles.percentValue}>{Math.floor(vote_average * 10)}
-                                    <span className={styles.percentIcon}>%</span>
-                                </p>
-                                <CircularProgress style={{ color: progressColor(vote_average * 10), backgroundColor: 'rgb(8,28,34)', borderRadius: '50%', position: 'relative', zIndex: '2' }} variant="determinate" value={Math.round(vote_average * 10)} thickness={3} size={70} />
-                            </div>
-                            <p className={styles.userScoreLabel}>User<br />Score</p>
+        <>
+            <div className={clsx(styles.imageMobile)} style={{ backgroundImage: `url(https://www.themoviedb.org/t/p/w1000_and_h450_multi_faces${backdrop_path})` }}>
+                <Image className={styles.poster} src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${poster_path}`} alt={original_title} width={98} height={150} />
+            </div>
+            <div className={styles.containerFluid} style={{ backgroundImage: `url(https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${backdrop_path})` }}>
+                <div className={styles.background}></div>
+                <div className={styles.container}>
+                    <Image className={styles.poster} src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${poster_path}`} alt={original_title} width={300} height={450} />
+                    <div className={styles.movieInfoContainer}>
+                        <h1>{original_title || original_name}</h1>
+                        <div className={styles.info}>
+                            <div className={styles.certification}>{adult ? 'M' : 'PG-13'}</div>
+                            •<p className={styles.languages}>{release_date || first_air_date} {`(${origin_country[0] || original_language})`}</p>
+                            •<p>{renderGenres()}</p>
+                            •{<p>{runtime ? calculateRuntime(runtime) : calculateRuntime(episode_run_time[0])}</p>}
                         </div>
-                        {trailerVideo.id && <button className={styles.playTrailer} onClick={openTrailer}>
-                            <Icon component={PlayArrowIcon} />
-                            Play Trailer
-                        </button>}
-                    </div>
-                    <p className={styles.quotes}>{tagline}</p>
-                    <div className={styles.overviewContainer}>
-                        <h3 className={styles.overviewTitle}>Overview</h3>
-                        <p className={styles.overview}>{overview}</p>
-                    </div>
-                    <div className={styles.directorContainer}>
-                    {renderCrew()}
+                        <div className={styles.scoreAndTrailer}>
+                            <div className={styles.userScoreContainer}>
+                                <div className={styles.progress}>
+                                    <p className={styles.percentValue}>{Math.floor(vote_average * 10)}
+                                        <span className={styles.percentIcon}>%</span>
+                                    </p>
+                                    <CircularProgress style={{ color: progressColor(vote_average * 10), backgroundColor: 'rgb(8,28,34)', borderRadius: '50%', position: 'relative', zIndex: '2' }} variant="determinate" value={Math.round(vote_average * 10)} thickness={3} size={70} />
+                                </div>
+                                <p className={styles.userScoreLabel}>User<br />Score</p>
+                            </div>
+                            {trailerVideo.id && <button className={styles.playTrailer} onClick={openTrailer}>
+                                <Icon component={PlayArrowIcon} />
+                                Play Trailer
+                            </button>}
+                        </div>
+                        <p className={styles.quotes}>{tagline}</p>
+                        <div className={styles.overviewContainer}>
+                            <h3 className={styles.overviewTitle}>Overview</h3>
+                            <p className={styles.overview}>{overview}</p>
+                        </div>
+                        <div className={styles.directorContainer}>
+                            {renderCrew()}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
