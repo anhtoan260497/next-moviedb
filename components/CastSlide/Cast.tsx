ebacaceda76de0351cba987e4ce452058532d195
anhtoan260497 <anhtoan260497@gmail.com>
2023-03-10 21:18:53 +0700

    finish mobile movie detail header

---
+++ components/CastSlide/Cast.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from 'Cast.module.scss'
+
+function Cast() {
+    return (
+        <div>
+            Cast Slide
+        </div>
+    );
+}
+
+export default Cast;
